ab5395f9c3e607dfaa9ce974864f7c67a189fa40
Jean Gomes <jeangp2006@gmail.com>
2024-02-29 20:25:47 -0300

    config api in dev

--- quasar.config.js
+++ quasar.config.js
@@ -84,8 +84,15 @@ module.exports = configure(function (/* ctx */) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#devServer
     devServer: {
+      port: 8080,
       // https: true
-      open: true // opens browser window automatically
+      open: false, // opens browser window automatically
+      proxy: {
+        '/api': {
+          target: 'http://192.168.0.124:8001',
+          pathRewrite: { '^/api': 'api' }
+        }
+      }
     },
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#framework
