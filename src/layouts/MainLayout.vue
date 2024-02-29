14655346f66b5c9f31bc41dfe88172d313574c6b
Jean Gomes <jeangp2006@gmail.com>
2024-02-29 20:24:32 -0300

    add links

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -30,12 +30,43 @@
         >
           Essential Links
         </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
+        <q-item
+          :to="{name: 'purchases'}"
+        >
+          <q-item-section
+            avatar
+          >
+            <q-icon name="money" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Compras</q-item-label>
+            <q-item-label caption>Listagem</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item
+          :to="{name: 'newPurchase'}"
+        >
+          <q-item-section
+            avatar
+          >
+            <q-icon name="money" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Cadastrar Compra</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item
+          :to="{name: 'purchaseItems'}"
+        >
+          <q-item-section
+            avatar
+          >
+            <q-icon name="money" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Lista items</q-item-label>
+          </q-item-section>
+        </q-item>
       </q-list>
     </q-drawer>
 
@@ -47,52 +78,6 @@
 
 <script setup lang="ts">
 import { ref } from 'vue'
-import EssentialLink, { EssentialLinkProps } from 'components/EssentialLink.vue'
-
-const essentialLinks: EssentialLinkProps[] = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
 
 const leftDrawerOpen = ref(false)
 
